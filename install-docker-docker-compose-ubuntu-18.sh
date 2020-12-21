c0a86e6fe54899994684e0dd954380585cd1e51c
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2020-12-21 14:36:01 +0100

    Create install-docker-docker-compose-ubuntu-18.sh

---
+++ install-docker-docker-compose-ubuntu-18.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "installing Docker"
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+
+echo "installing Docker Compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
