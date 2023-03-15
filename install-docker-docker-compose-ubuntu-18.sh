67d286f6928a17a7973125b17cb8989c238b7168
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2023-03-15 11:24:05 +0100

    Update install-docker-docker-compose-ubuntu-18.sh

--- install-docker-docker-compose-ubuntu-18.sh
+++ install-docker-docker-compose-ubuntu-18.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "installing Docker"
+echo "installing Docker in ubuntu"
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -7,7 +7,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y docker-ce
 
-echo "installing Docker Compose"
+echo "installing Docker Compose in ubuntu "
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
